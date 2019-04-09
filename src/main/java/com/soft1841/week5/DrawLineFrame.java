46f0d23ecc48718a59fe35da838cd6715dd79526
Kchenyuhang <kchengyuhang@gmail.com>
2019-04-10 06:36:06 +0800

    打卡练习

---
+++ src/main/java/com/soft1841/week5/DrawLineFrame.java
@@ -0,0 +1,26 @@
+package com.soft1841.week5;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawLineFrame extends JFrame {
+    public DrawLineFrame(){
+        Init();
+        setTitle("彩绘彩色线段");
+        setSize(1000,600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+    private void Init(){
+        getContentPane().setBackground(new Color(0,0,0));
+        DrawLineThread drawLineThread = new DrawLineThread();
+        drawLineThread.setFrame(this);
+        new Thread(drawLineThread).start();
+    }
+
+    public static void main(String[] args) {
+        new DrawLineFrame();
+    }
+}
