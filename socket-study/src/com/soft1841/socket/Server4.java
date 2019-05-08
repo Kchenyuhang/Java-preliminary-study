adbb78bc98487625c01caaf9ab1ef42bb4dce353
Kchenyuhang <kchengyuhang@gmail.com>
2019-05-09 06:17:26 +0800

    Java 网络编程(四）

---
+++ socket-study/src/com/soft1841/socket/Server4.java
@@ -0,0 +1,41 @@
+package com.soft1841.socket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server4 {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(10086);
+        System.out.println("服务器启动...");
+        while (true) {
+            Socket socket = serverSocket.accept();
+            ServerThread4 server = new ServerThread4(socket);
+            new Thread(server).start();
+        }
+    }
+
+    static class ServerThread4 implements Runnable {
+        private Socket socket;
+        public ServerThread4(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("客户端"+ socket.getInetAddress() + "连接成功！");
+            try {
+                File file = new File("D:\\1.jpg");
+                InputStream inputStream = new FileInputStream(file);
+                byte[] bytes = new byte[1024];
+                inputStream.read(bytes);
+                OutputStream outputStream = socket.getOutputStream();
+                outputStream.write(bytes);
+                inputStream.close();
+                outputStream.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
